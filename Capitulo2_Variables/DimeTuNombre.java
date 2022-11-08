c6a6cf3b0942d035942c049b37065e1e28709b24
Pablojavier22 <pmungar408@g.educaand.es>
2022-11-08 15:48:08 +0100

    1

--- Capitulo2_Variables/DimeTuNombre.java
+++ Capitulo2_Variables/DimeTuNombre.java
@@ -1,9 +1,12 @@
 public class DimeTuNombre {
     public static void main(String[] args) {
         String nombre;
+		String apellido;
 
         System.out.print("Por favor, dime cómo te llamas: ");
         nombre = System.console().readLine();
-        System.out.println("Hola " + nombre + ", ¡encantado de conocerte!");
+		System.out.print("Dime tu primer apellido: ");
+		apellido = System.console().readLine();
+        System.out.println("Hola " + nombre + " " + apellido + ", ¡encantado de conocerte!");
     }
 }
